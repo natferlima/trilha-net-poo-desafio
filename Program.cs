b6e5821ea823dac0111a70413e4045d0afc96071
natferlima <ferlima.natalia@gmail.com>
2022-10-17 11:48:17 -0300

    completando o desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("987654322", "Modelo1", "1111111111", 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
+
+
+Console.WriteLine("Smartphone Nokia: ");
+Nokia nokia = new Nokia("976543218", "Modelo2", "2222222222", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
